688608e3718e51058f864b6a21e14cd2e6b8e763
AnduinXue <anduin@aiursoft.com>
2024-01-30 18:41:04 +0000

    Fix builds.

--- src/Aiursoft.GptGateway.Api/Services/PostRequest/MockModelMiddleware.cs
+++ src/Aiursoft.GptGateway.Api/Services/PostRequest/MockModelMiddleware.cs
@@ -5,7 +5,7 @@ namespace Aiursoft.GptGateway.Api.Services.PostRequest;
 
 public class MockModelMiddleware : IPostRequestMiddleware
 {
-    public Task<CompletionData> PostRequest(HttpContext context, OpenAiModel model, CompletionData data, DateTime requestTime)
+    public Task<CompletionData> PostRequest(HttpContext context, OpenAiModel model, CompletionData data, DateTime requestTime, ConversationContext conv)
     {
         data.Model = "aiursoft-chat";
         return Task.FromResult(data);
