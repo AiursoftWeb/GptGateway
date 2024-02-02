514477f3b498b23277a639ad5b29726da3238a1b
AnduinXue <anduin@aiursoft.com>
2024-02-02 06:53:25 +0000

    Replace out meaningless words.

--- src/Aiursoft.GptGateway.Api/Services/Plugins/SearchPlugin.cs
+++ src/Aiursoft.GptGateway.Api/Services/Plugins/SearchPlugin.cs
@@ -6,7 +6,9 @@ namespace Aiursoft.GptGateway.Api.Services.Plugins;
 public class SearchPlugin : IPlugin
 {
     public string PluginName => "搜索插件";
-    
+
+    private static readonly string[] BadWords = new string[] {"今天","昨天","明天","现在","这里","那里","这个","那个","这种","那种","这样","那样","这么","那么","这些","那些","当时","当地","当天","当年","当月","当周","如何","什么","哪里","哪个","哪种","哪样","哪么","哪些","怎么","怎么样","怎么着","怎么办","怎么会","怎么说","怎么做","怎么了"};
+
     private const string ShouldUse =
         "你是一个旨在解决人类问题的人工智能。现在我遇到了一个问题\n\n```\n{0}\n```\n\n虽然你的知识是固定的，但是面对一些需要实时性信息和关于一些复杂的、可能变化的对象，我们可以使用搜索引擎查找一些关键词汇来得到更多背景知识，再结合搜索的结果得出答案。\n\n当然，如果只是问候、翻译等，这类不需要借助搜索引擎的工作，我们可以正常回答。\n\n现在，我需要你帮我判断，为了解决这个问题，是否需要先用搜索引擎来调查一些实体的知识。如果应当，请输出 `true`，否则请输出 `false`。不要输出其它内容。";
 
@@ -57,7 +59,12 @@ public class SearchPlugin : IPlugin
             out var rawQuestion,
             mergeAsOne: true);
         var textToSearchObject = await _openAiService.AskModel(requestModel, GptModel.Gpt4);
-        var textToSearch = textToSearchObject.Choices.FirstOrDefault()!.Message!.Content!.Trim('\"').Trim();
+        var textToSearch = textToSearchObject.Choices.FirstOrDefault()!.Message!.Content!
+            .Trim('\"')
+            .Trim();
+
+        textToSearch = BadWords.Aggregate(textToSearch, (current, badWord) => current.Replace(badWord, " "));
+
         _logger.LogInformation("Search plugin needs to search: {0}", textToSearch);
         
         context.PluginMessages.Add($@"> 使用搜索引擎搜索了：""{textToSearch}"".");
