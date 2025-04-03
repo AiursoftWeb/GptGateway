bc5e0bada421464f94d3e187b0a030b1758f7e0a
Anduin Xue <anduin@aiursoft.com>
2025-04-03 07:23:08 +0000

    Remove unused CopyProxyHttpResponse method
    The CopyProxyHttpResponse method was no longer in use and has been removed to clean up the codebase and improve maintainability. This change simplifies the ProxyController by eliminating unnecessary code.

--- src/Aiursoft.GptGateway/Controllers/ProxyController.cs
+++ src/Aiursoft.GptGateway/Controllers/ProxyController.cs
@@ -121,36 +121,6 @@ public class ProxyController(
             return Ok(response);
         }
     }
-
-    private static async Task CopyProxyHttpResponse(HttpContext context, HttpResponseMessage responseMessage)
-    {
-        if (responseMessage == null)
-        {
-            throw new ArgumentNullException(nameof(responseMessage));
-        }
-
-        var response = context.Response;
-
-        response.StatusCode = (int)responseMessage.StatusCode;
-        foreach (var header in responseMessage.Headers)
-        {
-            response.Headers[header.Key] = header.Value.ToArray();
-        }
-
-        foreach (var header in responseMessage.Content.Headers)
-        {
-            response.Headers[header.Key] = header.Value.ToArray();
-        }
-
-        // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
-        response.Headers.Remove("transfer-encoding");
-
-        using (var responseStream = await responseMessage.Content.ReadAsStreamAsync())
-        {
-            var streamCopyBufferSize = 81920;
-            await responseStream.CopyToAsync(response.Body, streamCopyBufferSize, context.RequestAborted);
-        }
-    }
 }
 
 public class ModelTag
